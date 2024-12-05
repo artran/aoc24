d5d0f373b3f76b4b2debf01e28075db8de963738
Ray Tran <ray@artran.co.uk>
2024-12-05 16:45:20 +0000

    Make a func to split the input strings

--- day1/part1.go
+++ day1/part1.go
@@ -3,6 +3,8 @@ package day1
 import (
 	"fmt"
 	"slices"
+	"strconv"
+	"strings"
 )
 
 func Distances(list1, list2 []int) int {
@@ -17,4 +19,24 @@ func Distances(list1, list2 []int) int {
 
 	fmt.Println(distance)
 	return distance
+}
+
+func SplitInput(input []string) ([]int, []int) {
+	var list1, list2 []int
+
+	for _, value := range input {
+		split := strings.Split(value, "   ")
+		val1, err := strconv.Atoi(split[0])
+		if err != nil {
+			panic(err)
+		}
+		val2, err := strconv.Atoi(split[1])
+		if err != nil {
+			panic(err)
+		}
+		list1 = append(list1, val1)
+		list2 = append(list2, val2)
+	}
+
+	return list1, list2
 }
--- day1/part1_test.go
+++ day1/part1_test.go
@@ -1,6 +1,9 @@
 package day1
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestDistances(t *testing.T) {
 	list1 := []int{3, 4, 2, 1, 3, 3}
@@ -11,5 +14,28 @@ func TestDistances(t *testing.T) {
 
 	if actual != expected {
 		t.Errorf("got %d want %d", actual, expected)
+	}
+}
+
+func TestLineSplitting(t *testing.T) {
+	input := []string{
+		"3   4",
+		"4   3",
+		"2   5",
+		"1   3",
+		"3   9",
+		"3   3",
+	}
+	expectedList1 := []int{3, 4, 2, 1, 3, 3}
+	expectedList2 := []int{4, 3, 5, 3, 9, 3}
+
+	list1, list2 := SplitInput(input)
+
+	if slices.Equal(list1, expectedList1) == false {
+		t.Errorf("First list is wrong: got %d, want %d", list1, expectedList1)
+	}
+
+	if slices.Equal(list2, expectedList2) == false {
+		t.Errorf("Second list is wrong: got %d, want %d", list2, expectedList2)
 	}
 }
