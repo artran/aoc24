2a51b3e6b6c236686f7bdb03fdde71363f62a38b
Ray Tran <ray@artran.co.uk>
2024-12-07 15:19:43 +0000

    Complete part1

--- day3/day3.go
+++ day3/day3.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"regexp"
 	"strconv"
 )
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	total := 0
+	for _, line := range lines {
+		total += DeCorrupt(line)
+	}
+
+	fmt.Printf("Total: %d", total)
+}
 
 func DeCorrupt(data string) int {
 	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
