c8261a1df478591e68d71f73f82f444c06ff1c03
Ray Tran <ray@artran.co.uk>
2024-12-07 15:41:22 +0000

    Refactor test to use structs

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -2,13 +2,21 @@ package main
 
 import "testing"
 
+type TestInput struct {
+	data     string
+	expected int
+}
+
 func TestDecorruption(t *testing.T) {
-	data := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-	expected := 161
+	test1 := TestInput{"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", 161}
+	tests := []TestInput{test1}
 
-	calculated := DeCorrupt(data)
+	for idx, test := range tests {
 
-	if calculated != expected {
-		t.Errorf("expected %d, got %d", expected, calculated)
+		calculated := DeCorrupt(test.data)
+
+		if calculated != test.expected {
+			t.Errorf("Test %d: expected %d, got %d", idx, test.expected, calculated)
+		}
 	}
 }
