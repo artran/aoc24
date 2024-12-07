8c6d4ee089750734f45f8e1faff68e7d3a74b8ec
Ray Tran <ray@artran.co.uk>
2024-12-07 15:14:08 +0000

    Pass tests

--- day3/day3.go
+++ day3/day3.go
@@ -1,5 +1,19 @@
 package main
 
-func DeCorrupt(string) int {
-	return 0
+import (
+	"regexp"
+	"strconv"
+)
+
+func DeCorrupt(data string) int {
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	found := re.FindAllStringSubmatch(data, -1)
+
+	total := 0
+	for _, match := range found {
+		lhs, _ := strconv.Atoi(match[1])
+		rhs, _ := strconv.Atoi(match[2])
+		total += lhs * rhs
+	}
+	return total
 }
