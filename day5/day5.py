90aec817b3a213bef7005cbe97539ad21323824c
Ray Tran <ray@artran.co.uk>
2024-12-15 13:11:24 +0000

    Parse the ordering rules

--- day5/day5.py
+++ day5/day5.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
 
-from typing import TextIO
+from collections import defaultdict
+from typing import List, TextIO
 
 
 def day5(input: TextIO):
     score = 0
 
-    for line in input:
-        line = line.strip()
+    rule_definitions = extract_rule_definitions(input)
+    rules = convert_defs_to_rules(rule_definitions)
 
     print(score)
+
+def extract_rule_definitions(input: TextIO) -> list[tuple[int, int]]:
+    rules = []
+
+    for line in input:
+        rule = line.split('|')
+        if len(rule) == 2:
+            rules.append(rule)
+
+    rules = map(lambda r: (int(r[0]), int(r[1])), rules)
+    return list(rules)
+
+def convert_defs_to_rules(defs: list[tuple[int,int]]) -> dict[tuple[int,int], bool]:
+    rules = defaultdict(lambda: True)
+
+    for definition in defs:
+        rules[(definition[1], definition[0])] = False
+
+    return rules
 
 
 if __name__ == '__main__':
---
+++ day5/test_day5.py
@@ -0,0 +1,36 @@
+import pytest
+
+from day5.day5 import convert_defs_to_rules, extract_rule_definitions
+
+
+def test_ordering_rules_length():
+    with open('test.txt') as input:
+        rules = extract_rule_definitions(input)
+
+    assert len(rules) == 21
+
+def test_rule_defs_type():
+    with open('test.txt') as input:
+        rules = extract_rule_definitions(input)
+
+    for rule in rules:
+        assert type(rule[0]) == int
+        assert type(rule[1]) == int
+
+def test_rule_values():
+    with open('test.txt') as input:
+        defs = extract_rule_definitions(input)
+        rules = convert_defs_to_rules(defs)
+
+    # Reversed ordering is denied
+    assert rules[(53, 47)] is False
+    assert rules[(13, 53)] is False
+    assert rules[(29, 61)] is False
+
+    # Forward ordering is allowed
+    assert rules[(47, 53)] is True
+    assert rules[(53, 13)] is True
+    assert rules[(61, 29)] is True
+
+    # Not mentioned ordering is allowed
+    assert rules[(12, 29)] is True
